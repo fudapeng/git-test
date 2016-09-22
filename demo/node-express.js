e5f34e5284e12d2ced0705dc5ec39ea105af654b
fudapeng <fdp5576175@163.com>
2016-09-22 09:33:45 +0800

    添加错误页面

--- demo/node-express.js
+++ demo/node-express.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 var multer  = require('multer');
 var bodParper = require('body-parser');
 var cookieParser = require('cookie-parser')
-
+var router = express.Router();
 
 
 // 创建 application/x-www-form-urlencoded 编码解析
@@ -15,13 +15,24 @@ app.use(bodParper.urlencoded({ extended: false }));
 app.use(multer({ dest: './tmp/'}).array('file'));
 app.use(cookieParser());
 
+app.set('views', path.join(__dirname, 'tpl'));// 指定视图所在的位置
+app.set('view engine', 'ejs');// 注册模板引擎
+
+app.get('/user/:id',function(req,res){
+	res.send()
+})
+
+
 app.get('/',function(req,res){
 	console.log(22);
 })
 
-app.get('/uploader.html',function(req,res){
+app.get('/uploader.html',function(req,res,next){
 	console.log('cookies%', req.cookies);
+	console.log(app.local)
+	console.log(admin.mountpath);
 	res.sendFile(__dirname + "/uploader.html");
+
 })
 //posh请求
 app.post('/test-post',urlencodedParser,function(req,res){
@@ -52,8 +63,14 @@ app.post('/test-upload', function (req, res) {
        });
    });
 })
+//错误处理
+app.use(function(err, req, res, next) {
+	console.log('err')
+  console.error(err.stack);
+  res.status(500).send('error');
+});
 
-var server = app.listen(3000, function () {
+var server = app.listen(3333, function () {
 
   var host = server.address().address
   var port = server.address().port
